8169b629c4acfd20fc38ae1cc4243b2b5e00d5d1
Thais <thaisflins@gmail.com>
2022-04-08 18:08:04 -0400

    Create simple item for news layout

--- app/src/main/java/com/thaislins/newsfeed/app/MainActivity.kt
+++ app/src/main/java/com/thaislins/newsfeed/app/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.newsfeed
+package com.thaislins.newsfeed.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.newsfeed.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/test/java/com/thaislins/newsfeed/ExampleUnitTest.kt
+++ app/src/test/java/com/thaislins/newsfeed/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.newsfeed
+package com.thaislins.newsfeed
 
 import org.junit.Test
 
