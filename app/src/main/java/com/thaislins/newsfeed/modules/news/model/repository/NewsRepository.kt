f8992899b0fe3dd94fa12b6ca8767075446c8443
Thais <thaisflins@gmail.com>
2022-04-11 22:03:13 -0400

    Refactor repository and view model

--- app/src/main/java/com/thaislins/newsfeed/modules/news/model/repository/NewsRepository.kt
+++ app/src/main/java/com/thaislins/newsfeed/modules/news/model/repository/NewsRepository.kt
@@ -6,16 +6,6 @@ import com.thaislins.newsfeed.service.NewsService
 class NewsRepository(private val newsService: NewsService) {
 
     suspend fun getNewsList(): List<News>? {
-        return try {
-            val response = newsService.getNewsList()
-
-            if (response != null) {
-                response
-            } else {
-                throw Exception()
-            }
-        } catch (ex: Exception) {
-            throw Exception()
-        }
+        return newsService.getNewsList()
     }
 }
--- app/src/main/java/com/thaislins/newsfeed/modules/news/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/thaislins/newsfeed/modules/news/viewmodel/NewsViewModel.kt
@@ -1,28 +1,28 @@
 package com.thaislins.newsfeed.modules.news.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.thaislins.newsfeed.modules.news.model.News
+import com.thaislins.newsfeed.modules.news.model.Resource
 import com.thaislins.newsfeed.modules.news.model.repository.NewsRepository
 import kotlinx.coroutines.launch
 
 class NewsViewModel(private val repository: NewsRepository) : ViewModel() {
 
-    private val _newsList = MutableLiveData<List<News>?>()
-    private var _temp = MutableLiveData<List<News>?>()
-    val newsList: LiveData<List<News>?> = _temp
+    private val _newsList = MutableLiveData<Resource<List<News>?>>() // original list
+    private var _temp = MutableLiveData<Resource<List<News>?>>() // list that may be changed according to filters
+    val newsList: LiveData<Resource<List<News>?>> = _temp
 
     fun loadNewsList() {
         viewModelScope.launch {
             try {
-                _temp.value = repository.getNewsList()
-                _newsList.value = _temp.value
+                _temp.value = Resource(data = repository.getNewsList(), hasError = false)
             } catch (ex: Exception) {
-                ex.message?.let { Log.e("Error", it) }
+                _temp.value = Resource(data = null, hasError = true)
             }
+            _newsList.value = _temp.value
         }
     }
 
@@ -31,7 +31,7 @@ class NewsViewModel(private val repository: NewsRepository) : ViewModel() {
             _temp.value = _newsList.value
         } else {
             _temp.value =
-                _newsList.value?.filter { it.type == type }
+                Resource(data = _newsList.value?.data?.filter { it.type == type }, hasError = _newsList.value?.hasError)
         }
     }
 }
