46d494693bec8222da0f73ff9f2b53a38d65e881
Thais <thaisflins@gmail.com>
2022-04-13 03:16:43 -0400

    Add splashscreen with animation

--- app/src/main/java/com/thaislins/newsfeed/data/local/NewsDB.kt
+++ app/src/main/java/com/thaislins/newsfeed/data/local/NewsDB.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.thaislins.newsfeed.modules.news.model.News
 
-@Database(entities = [News::class], version = 1)
+@Database(entities = [News::class], version = 1, exportSchema = false)
 abstract class NewsDB : RoomDatabase() {
 
     abstract fun newsDao(): NewsDao
---
+++ app/src/main/java/com/thaislins/newsfeed/modules/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package com.thaislins.newsfeed.modules.splashscreen
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.thaislins.newsfeed.R
+import com.thaislins.newsfeed.modules.news.view.NewsActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashScreenActivity, NewsActivity::class.java))
+            finish()
+        }, 5000)
+    }
+}
